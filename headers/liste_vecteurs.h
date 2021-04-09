7084addad967ae758aa0249d62a0e2e84c45c3d6
Samy206 <samy.boumali@hotmail.com>
2021-04-09 16:02:07 +0200

    Premier jet liste_vecteur

--- headers/liste_vecteurs.h
+++ headers/liste_vecteurs.h
@@ -1,4 +1,4 @@
-#include "vecteur.h"
+#include "vecteurs.h"
 
 #ifndef LISTE_VECTEURS
 #define LISTE_VECTEURS
@@ -6,24 +6,24 @@
 typedef struct element        //Brique de la liste appelée "element" : Un vecteur et un pointeur vers le suivant
 {
     Vector vecteur;
-    Vector * suivant;
+    struct element * suivant;
 }
 element;
 
 typedef struct liste          //Liste à proprement parler
 {
-    elem * premier;
+    element * premier;
     int taille;
 }
 Liste_vecteur;
 
-Liste_vecteur * initialisation_liste();                          //initialisation
+void initialisation_liste(Liste_vecteur * liste);                //initialisation
 
 void push_liste(Liste_vecteur * liste, Vector vec);              //Ajout d'un vecteur à la liste
 Vector top_liste(Liste_vecteur * liste);                         //Consultation du dernier vecteur ajouté
 void pop_liste(Liste_vecteur * liste);                           //Suppression du dernier élément ajouté
 Vector consultation_liste(Liste_vecteur * liste, int indice);    //Consultation d'un vecteur dans la liste
-void free_liste(Liste_vecteur liste);                            //Libération de la mémoire allouée
+void free_liste(Liste_vecteur* liste);                            //Libération de la mémoire allouée
 
 
 
