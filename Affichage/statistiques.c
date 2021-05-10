31fc10f16bde3bd822c991b3f78ddd68de2af028
Ennaox <55583079+Ennaox@users.noreply.github.com>
2021-05-10 17:16:41 +0200

    Ajout calcul systématique min et max

--- Affichage/statistiques.c
+++ Affichage/statistiques.c
@@ -9,15 +9,8 @@ statistiques init_statistiques(statistiques stat, size_t nb_dim, int* statistiqu
         stat.statistiques_demandees[i] = statistiques_demandes[i];
     }
 
-    if(statistiques_demandes[0])
-        stat.min_d = malloc(nb_dim* sizeof(double));
-    else
-        stat.min_d = NULL;
-
-    if(statistiques_demandes[1])
-        stat.max_d = malloc(nb_dim* sizeof(double));
-    else
-        stat.max_d = NULL;
+    stat.min_d = malloc(nb_dim* sizeof(double));
+    stat.max_d = malloc(nb_dim* sizeof(double));
 
     if(statistiques_demandes[2] || statistiques_demandes[3] || statistiques_demandes[4] || statistiques_demandes[5])
         stat.moy_d = malloc(nb_dim* sizeof(double));
@@ -232,11 +225,8 @@ statistiques calcul_des_statistiques(int* statistiques_demandes, Liste_vecteur* 
 	statistiques stat ;
 	stat = init_statistiques(stat,consultation_liste(vecteurs, 0).taille, statistiques_demandes);
 
-	if(statistiques_demandes[0])
-		stat = minimum(stat, vecteurs);
-
-	if(statistiques_demandes[1])
-		stat = maximum(stat, vecteurs);
+	stat = minimum(stat, vecteurs);
+	stat = maximum(stat, vecteurs);
 
 	if(statistiques_demandes[2] || statistiques_demandes[3] || statistiques_demandes[4] || statistiques_demandes[5])
 		stat = moyenne(stat, vecteurs);
@@ -253,29 +243,26 @@ statistiques calcul_des_statistiques(int* statistiques_demandes, Liste_vecteur* 
 	return stat;
 }
 
-char * to_string(statistiques stat, char * string)
+void to_string(statistiques stat, char * string)
 {
-
-
     if(stat.statistiques_demandees[0])
-    	strcat(string,"Minimum");
+        strcat(string,"Minimum");
 
     if(stat.statistiques_demandees[1])
-    	strcat(string,"|Maximum");
+        strcat(string,"|Maximum");
 
     if(stat.statistiques_demandees[2] || stat.statistiques_demandees[3] || stat.statistiques_demandees[4] || stat.statistiques_demandees[5])
-    	strcat(string,"|Average");
+        strcat(string,"|Average");
 
     if(stat.statistiques_demandees[3] || stat.statistiques_demandees[4] || stat.statistiques_demandees[5])
-    	strcat(string,"|Variance");
+        strcat(string,"|Variance");
 
     if(stat.statistiques_demandees[4] || stat.statistiques_demandees[5])
-    	strcat(string,"|Standard deviation");
+        strcat(string,"|Standard deviation");
 
     if(stat.statistiques_demandees[5])
-    	strcat(string,"|Autocorrelation");
+        strcat(string,"|Autocorrelation");
 
-    return string;
 };
 
 void affichage(statistiques stat, size_t n)
