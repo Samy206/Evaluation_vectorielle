c63ee37be42b83a3abd3dec9c014d81b68ebd21d
Samy206 <43828609+Samy206@users.noreply.github.com>
2021-05-10 22:55:54 +0200

    Gestion_ES Samy OK

--- Affichage/gestion_ES.h
+++ Affichage/gestion_ES.h
@@ -10,23 +10,24 @@ typedef struct gestion_es
 {
     char * fonction;                            //Fonction entrée par l'utilisateur
     Liste_vecteur * liste;                      //Liste de vecteurs formant la courbe
-    statistiques stats;                       //Liste des statistiques sur la courbe
+    statistiques * stats;                       //Liste des statistiques sur la courbe
     char * fic_gnup;                            //Chemin absolu du fichier contenant les coordonnées des vecteurs
     char * script_gnup;                         /*Chemin absolu du fichier contenant script utilisant les coordonnées
                                                    des vecteurs*/
     char * fic_post;                            //Chemin absolu du fichier contenant le script postscript affichant les statistiques
 }
-Gestion_ES;
+        Gestion_ES;
 
 
-Gestion_ES *initialisation_ES(char * fct);
+void initialisation_ES(Gestion_ES * gestionnaire,char * fct, Liste_vecteur * liste, statistiques * stats);
 
-int generation_script_gnuplot(Gestion_ES * gestionnaire, char* file_name, Liste_vecteur *list); //Génération script lancement de gnuplot
-int generation_fic_gnuplot(Gestion_ES * gestionnaire, char *file_name, Liste_vecteur * liste);    //Génération fichier de coordonnées gnup
+int generation_fic_gnuplot(Gestion_ES * gestionnaire, char *file_name);    //Génération fichier de coordonnées gnup
+
+int generation_script_gnuplot(Gestion_ES * gestionnaire, char* file_name); //Génération script lancement de gnuplot
+void * launch_gnup_script(void * filename); /*Lancement du script gnuplot afin de générer un graphe sous format png*/
 
 
-
-int generation_fic_postscript(Gestion_ES * gestionnaire, char * filename, Liste_vecteur *list, statistiques statis); //génération fichier postscript ( écriture statistiques )
+int generation_fic_postscript(Gestion_ES * gestionnaire, char * filename); //génération fichier postscript ( écriture statistiques )
 int draw_table_postscript(FILE * file,int x, int y, int largeur);          //Dessin de chaque partie du tableau
 int stat_string_parser(char * string,char ** tableau);                     //Récupération des stats à affichier
 char index_to_char(int index);                                             //Transforme un 0 par un X, un 1 par un Y etc
@@ -37,4 +38,3 @@ double * string_parser(char * entiers);                   //Transformation des l
 void free_gestionnaire_es(Gestion_ES * gestionnaire);      //Libération de la mémoire
 
 #endif
-
