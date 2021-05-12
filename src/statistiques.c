2589d14b5ead87d12e6dd0abed7378013cd349c4
Samy206 <43828609+Samy206@users.noreply.github.com>
2021-05-12 09:38:35 +0200

    Update statistiques.c

--- src/statistiques.c
+++ src/statistiques.c
@@ -1,16 +1,17 @@
-#include "../headers/statistiques.h"
+#include "Headers/statistiques.h"
 
-statistiques init_statistiques(statistiques stat, size_t nb_dim, int* statistiques_demandes)
+statistiques init_statistiques(size_t nb_dim, int* statistiques_demandes)
 {
+    statistiques stat;
     stat.statistiques_demandees = malloc(6*sizeof(int));
 
     for(int i=0;i<6;i++)
     {
         stat.statistiques_demandees[i] = statistiques_demandes[i];
     }
-    
+
     stat.min_d = malloc(nb_dim* sizeof(double));
-   	stat.max_d = malloc(nb_dim* sizeof(double));
+    stat.max_d = malloc(nb_dim* sizeof(double));
 
     if(statistiques_demandes[2] || statistiques_demandes[3] || statistiques_demandes[4] || statistiques_demandes[5])
         stat.moy_d = malloc(nb_dim* sizeof(double));
@@ -64,7 +65,7 @@ statistiques free_statistiques(statistiques stat)
 double calcul_norme(Vector v)
 {
 	double somme = 0;
-	for(int i=0; i < v.taille; i++)
+    for(int i=0; i < (int)v.taille; i++)
 	{
 		somme += pow(v.tableau[i],2);
 	}
@@ -76,7 +77,7 @@ statistiques minimum(statistiques stat, Liste_vecteur* vecteurs)
 	size_t nb_dim = consultation_liste(vecteurs, 0).taille;
 	stat.min_n = calcul_norme(consultation_liste(vecteurs,0));
 	
-	for(int i = 0; i<nb_dim; i++)
+    for(int i = 0; i<(int)nb_dim; i++)
 	{
 		stat.min_d[i] = consultation_liste(vecteurs,0).tableau[i];
 	}
@@ -87,7 +88,7 @@ statistiques minimum(statistiques stat, Liste_vecteur* vecteurs)
 		if(norme < stat.min_n)
 			stat.min_n = norme;
 		
-		for(int i = 0; i<nb_dim; i++)
+        for(int i = 0; i<(int)nb_dim; i++)
 		{
 			double dim = consultation_liste(vecteurs,w).tableau[i];
 			if(dim < stat.min_d[i])
@@ -103,7 +104,7 @@ statistiques maximum(statistiques stat, Liste_vecteur* vecteurs)
 	size_t nb_dim = consultation_liste(vecteurs, 0).taille;
 	stat.max_n = calcul_norme(consultation_liste(vecteurs,0));
 	
-	for(int i = 0; i<nb_dim; i++)
+    for(int i = 0; i<(int)nb_dim; i++)
 	{
 		stat.max_d[i] = consultation_liste(vecteurs,0).tableau[i];
 	}
@@ -114,7 +115,7 @@ statistiques maximum(statistiques stat, Liste_vecteur* vecteurs)
 		if(norme > stat.max_n)
 			stat.max_n = norme;
 		
-		for(int i = 0; i<nb_dim; i++)
+        for(int i = 0; i<(int)nb_dim; i++)
 		{
 			double dim = consultation_liste(vecteurs,w).tableau[i];
 			if(dim > stat.max_d[i])
@@ -132,7 +133,7 @@ statistiques moyenne(statistiques stat, Liste_vecteur* vecteurs)
 	{
 		stat.moy_n += calcul_norme(consultation_liste(vecteurs,w));
 
-		for(int i = 0; i<nb_dim; i++)
+        for(int i = 0; i<(int)nb_dim; i++)
 		{
 			double dim = consultation_liste(vecteurs,w).tableau[i];
 			stat.moy_d[i] += dim;
@@ -141,7 +142,7 @@ statistiques moyenne(statistiques stat, Liste_vecteur* vecteurs)
 
 	stat.moy_n = stat.moy_n/vecteurs->taille;
 
-	for(int i = 0; i<nb_dim; i++)
+    for(int i = 0; i<(int)nb_dim; i++)
 	{
 		stat.moy_d[i] = stat.moy_d[i]/vecteurs->taille;
 	}
@@ -184,7 +185,7 @@ statistiques ecart_type(statistiques stat, Liste_vecteur* vecteurs)
 	size_t nb_dim = consultation_liste(vecteurs, 0).taille;
 	stat.ect_n = sqrt(stat.var_n);
 	
-	for(int i = 0; i<nb_dim; i++)
+    for(int i = 0; i<(int)nb_dim; i++)
 	{
 		stat.ect_d[i] = sqrt(stat.var_d[i]);
 	}
@@ -200,7 +201,7 @@ statistiques calcul_auto_correlation(statistiques stat, Liste_vecteur* vecteurs,
 	size_t nb_dim = consultation_liste(vecteurs, 0).taille;
 	for(int w = 0; w<vecteurs->taille; w++)
 	{
-		for(int i = 0; i<nb_dim; i++)
+        for(int i = 0; i<(int)nb_dim; i++)
 		{
 			double dim1 = consultation_liste(vecteurs,w).tableau[i];
 			double dim2;
@@ -215,7 +216,7 @@ statistiques calcul_auto_correlation(statistiques stat, Liste_vecteur* vecteurs,
 			stat.atc_d[i] += (dim1 - stat.moy_d[i]) * (dim2 - stat.moy_d[i]);
 		}
 	}
-	for(int i=0; i<nb_dim; i++)
+    for(int i=0; i<(int)nb_dim; i++)
 		stat.atc_d[i] = (stat.atc_d[i]/vecteurs->taille)/stat.var_d[i];
 	return stat;
 }
@@ -223,7 +224,7 @@ statistiques calcul_auto_correlation(statistiques stat, Liste_vecteur* vecteurs,
 statistiques calcul_des_statistiques(int* statistiques_demandes, Liste_vecteur* vecteurs)
 {
 	statistiques stat ;
-	stat = init_statistiques(stat,consultation_liste(vecteurs, 0).taille, statistiques_demandes);
+	stat = init_statistiques(consultation_liste(vecteurs, 0).taille, statistiques_demandes);
 
 	stat = minimum(stat, vecteurs);
 	stat = maximum(stat, vecteurs);
@@ -243,28 +244,71 @@ statistiques calcul_des_statistiques(int* statistiques_demandes, Liste_vecteur* 
 	return stat;
 }
 
-char * to_string(statistiques stat)
+void to_string(statistiques stat, char * string)
 {
-    char * string = malloc(68*sizeof(char));
-    memset(string,0, 68*sizeof(char));
-
     if(stat.statistiques_demandees[0])
-    	strcat(string,"Minimum");
+        strcat(string,"Minimum");
 
     if(stat.statistiques_demandees[1])
-    	strcat(string,"|Maximum");
+        strcat(string,"|Maximum");
 
     if(stat.statistiques_demandees[2] || stat.statistiques_demandees[3] || stat.statistiques_demandees[4] || stat.statistiques_demandees[5])
-    	strcat(string,"|Average");
+        strcat(string,"|Average");
 
     if(stat.statistiques_demandees[3] || stat.statistiques_demandees[4] || stat.statistiques_demandees[5])
-    	strcat(string,"|Variance");
+        strcat(string,"|Variance");
 
     if(stat.statistiques_demandees[4] || stat.statistiques_demandees[5])
-    	strcat(string,"|Standard deviation");
+        strcat(string,"|Standard deviation");
 
     if(stat.statistiques_demandees[5])
-    	strcat(string,"|Autocorrelation");
+        strcat(string,"|Autocorrelation");
 
-    return string;
 };
+
+void affichage(statistiques stat, size_t n)
+{
+    if(stat.statistiques_demandees[0])
+    {
+        printf("Norme minimal:%.16f \n", stat.min_n);
+        for(int i=0; i<(int)n; i++)
+            printf("Minimal sur dim%d :%.16f |",i, stat.min_d[i]);
+        printf("\n");
+    }
+    if(stat.statistiques_demandees[1])
+    {
+        printf("Norme max:%.16f \n", stat.max_n);
+        for(int i=0; i<(int)n; i++)
+            printf("Max sur dim%d :%.16f |",i, stat.max_d[i]);
+        printf("\n");
+    }
+    if(stat.statistiques_demandees[2])
+    {
+        printf("Norme moy:%.16f \n", stat.moy_n);
+        for(int i=0; i<(int)n; i++)
+            printf("Moyenne sur dim%d :%.16f |",i, stat.moy_d[i]);
+        printf("\n");
+    }
+    if(stat.statistiques_demandees[3])
+    {
+        printf("Variance Norme:%lf \n", stat.var_n);
+        for(int i=0; i<(int)n; i++)
+            printf("Variance sur dim%d :%.16f |",i, stat.var_d[i]);
+        printf("\n");
+    }
+    if(stat.statistiques_demandees[4])
+    {
+        printf("Ecart-type Norme:%.16f \n", stat.ect_n);
+        for(int i=0; i<(int)n; i++)
+            printf("Ecart-type sur dim%d :%.16f |",i, stat.ect_d[i]);
+        printf("\n");
+    }
+    if(stat.statistiques_demandees[5])
+    {
+        for(int i=0; i<(int)n; i++)
+            printf("Auto-correlation sur dim%d :%.16f |",i, stat.atc_d[i]);
+        printf("\n");
+
+    }
+   //to_string(stat),string);
+}
