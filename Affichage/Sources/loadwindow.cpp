57350ef71dc6b3a50ab7c753e750210e3f9b135d
Samy206 <43828609+Samy206@users.noreply.github.com>
2021-05-17 17:51:47 +0200

    chargement fichier ges ok

--- Affichage/Sources/loadwindow.cpp
+++ Affichage/Sources/loadwindow.cpp
@@ -1,5 +1,5 @@
-#include "Headers/loadwindow.h"
 #include "ui_loadwindow.h"
+#include "../Headers/loadwindow.h"
 #include "iostream"
 #include "QMessageBox"
 #ifdef WINDOWS
@@ -11,7 +11,8 @@
 #endif
 
 extern "C" {
-#include "../Headers/chargement_fichier.h"
+#include "../Headers/gestion_ES.h"
+#include "../Headers/vecteurs.h"
 }
 
 loadwindow::loadwindow(QWidget *parent) :
@@ -37,40 +38,37 @@ void loadwindow::Validate()
     strcpy(chemin, path.c_str());
     strcat(chemin,".txt");
     std::cout << chemin << std::endl;
+    Vector vecteur1;
+    char function[50];
 
-    donnee test;
-    taille t;
-    t = init_struct_t(t);
-    test = init_struct_d(test);
-    test = chargement_fic_gnup(chemin,test,t);
+    int check = chargement_fic_gnup(&vecteur1,function,chemin);
 
-    if(test.erreur == 2)
+    if(check == 2)
     {
-        QMessageBox::critical(this, "File error", "The file canno't be open !");
+        QMessageBox::critical(this, "File error", "The file cannot be open !");
         return;
     }
 
-    if(test.erreur == 1)
+    if(check == 1)
     {
         QMessageBox::critical(this, "File error", "The file is not writen with the good format !");
         return;
     }
-    //free_struct_d(test,test.nombre);
 
 
     //mettre sous forme de fonction
-    funct = test.fonction;
+    funct = function;
     funct.pop_back();
     funct.erase(0,1);
 
 
     //mettre sous forme de vecteur
     vecteur = "U = (";
-    for (int i = 0; i < test.nombre/2; i++)
+    for (int i = 0; i < vecteur1.taille ; i++)
     {
-        std::string tmp = std::to_string(test.valeur[i]);
-        std::cout << test.valeur[i] << std::endl;
-        vecteur+= tmp;
+        std::string tmp = std::to_string(vecteur1.tableau[i]);
+        std::cout << vecteur1.tableau[i] << std::endl;
+        vecteur += tmp;
         vecteur += ",";
     }
     vecteur.pop_back();
