3fd1eb681b637fee12e7614d910376036c358d3a
Ennaox <55583079+Ennaox@users.noreply.github.com>
2021-05-24 18:06:15 +0200

    Update transfert_fichiers_affichage3D.h

--- Affichage/Headers/transfert_fichiers_affichage3D.h
+++ Affichage/Headers/transfert_fichiers_affichage3D.h
@@ -7,7 +7,7 @@
 #include <fstream>
 
 void transfert_stat(size_t nb_dim, std::string nom, statistiques stat);
-void transfert_vecteurs(std::string nom, Liste_vecteur *l);
+void transfert_vecteurs(std::string nom, Liste_vecteur *l, std::string departur);
 void transfert_fonction(std::string nom, std::string equation);
 
 #endif
